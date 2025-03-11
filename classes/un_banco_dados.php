c8c91e2f624cb424f19d23fc19d5668e01d01794
netmania1 <otacilio.ribeiro@gmail.com>
2025-03-11 14:34:13 -0300

    modified:   classes/un_banco_dados.php

--- classes/un_banco_dados.php
+++ classes/un_banco_dados.php
@@ -9,7 +9,7 @@
 	' DATA DE CRIAÇÃO     : 11/03/2025
 	' DATA DE ALTERAÇÃO   : 
 	' OBSERVAÇÕES         : Classe com parte de conexão com banco de dados e outras rotinas
-    '                       parecidas
+	'						parecidas
 	'***********************************************************************************************
 */
 class clsBancoDados{}
